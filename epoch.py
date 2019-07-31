1c53beabfc6e44a2449ba97bab964eb6ee25888b
Hugo Lopez <hlopez@nitelusa.com>
2019-07-31 13:25:01 -0500

    removed unnecessary test .py stuff

--- epoch.py
+++
@@ -1,9 +0,0 @@
-import json
-from datetime import datetime, timedelta
-
-token_data = open('.spotipyoauthcache', 'r').read()
-token_expiration = json.loads(token_data)['expires_at']
-current_time = int(datetime.now().timestamp())
-print(token_expiration)
-print(current_time)
-print(current_time > token_expiration)
