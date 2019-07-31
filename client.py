9993b3535d06677da3633c433d75511f20b9069c
hlopez <95hlopez@gmail.com>
2019-07-31 14:11:49 -0500

    added requirements

--- client.py
+++ client.py
@@ -8,6 +8,7 @@ from datetime import datetime
 #what if we pass time as parameter into spoticli object
 #spoticli checks preloop if current time > creation time + 40
 #if it is, exit, and have client re-loop it.
-		
+#i don't see a point to this. all it does is start spoticli
+
 if __name__ == '__main__':
 	SpotiCLI().cmdloop()
