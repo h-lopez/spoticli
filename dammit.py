87071e2c2f138a3b14621250e6845dd2939d8a8f
Hugo <95hlopez@gmail.com>
2019-10-21 16:17:21 -0500

    another test file please ignore thanks

---
+++ dammit.py
@@ -0,0 +1,20 @@
+from spotipy import Spotify
+from spotipy.scope import every
+from spotipy.util import prompt_for_user_token
+
+client_id = 'ad61a493657140c8a663f8db17730c4f'
+client_secret = '3c403975a6874b238339db2231864294'
+redirect_uri = 'http://localhost'
+
+token = prompt_for_user_token(
+    client_id,
+    client_secret,
+    redirect_uri,
+    scope=every
+)
+
+s = Spotify(token)
+
+tracks = s.current_user_top_tracks(limit=10)
+for track in tracks.items:
+    print(track.name)
