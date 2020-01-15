98f696ede8e55ead8d6e702fcc892b57b187035e
Hugo <95hlopez@gmail.com>
2020-01-15 15:53:36 -0600

    web server stuff and also added more stuff to .ignore

--- .history/auth_20200106144805.py
+++
@@ -1,1 +0,0 @@
-from spotipy import util
--- .history/auth_20200106153226.py
+++
@@ -1,9 +0,0 @@
-from spotipy import util
-
-conf = util.credentials_from_environment()
-app_token = util.request_client_token(*conf[:2])
-user_token = util.prompt_for_user_token(*conf)
-
-# Save the refresh token to avoid authenticating again
-refresh_token = ...     # Load refresh token
-user_token = util.refresh_user_token(*conf[:2], refresh_token)
