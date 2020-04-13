b22de1f7f1b679388b42632f3d2937f17895b9e3
Hugo <95hlopez@gmail.com>
2020-04-13 15:44:13 -0500

    verified auth works, working on implementing new API calls

--- __init__.py
+++ __init__.py
@@ -3,16 +3,11 @@ from tekore import Spotify, util, scope
 import auth
 
 if __name__ == '__main__':
+
+    ##load local creds
+    ##retrieve token using local creds
     cred = util.config_from_file('conf.spoticli')
     token = auth.prompt_for_user_token(*cred, scope=scope.every)
-    print(token)
-
-    #print(spotify.curre)
-
-    #racks = spotify.current_user_top_tracks(limit=10)
-    #potify.playback_start_tracks([t.id for t in tracks.items])
-
-    ### auth user
-    ### pass along auth token to spoticli
+    ### pass token to spoticli, spoticli with instantiate spotify object and handle refreshing
     ### spoticli will handle auth user and periodically refresh token as needed
     SpotiCLI().cmdloop()
