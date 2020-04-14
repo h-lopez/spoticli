591940c5edace1057259d5e7adb7d86334b7b8b8
Hugo <95hlopez@gmail.com>
2020-04-14 10:04:08 -0500

    first steps in implementing new functions

--- __init__.py
+++ __init__.py
@@ -9,6 +9,27 @@ if __name__ == '__main__':
     ##need to convert to tekore friendly format before we pass it along
     #scope = 'user-library-read user-library-modify user-read-currently-playing user-read-playback-state user-modify-playback-state user-read-recently-played playlist-read-private'
 
+    ### procedure
+    '''
+    if (cached_token_exists)
+        token = cached_token
+        ##skip directly to authentication portion
+    check if conf file exists...
+
+        exist = check_if_exists()
+        valid = check_if_valid()
+
+        if(!valid || !exist) #if file invalid (ie. unreadable) or doesn't exist
+            try:
+                blank/create it
+            except:
+                print error and exit program
+
+            creds.prompt(client_id, secret) #redirect uri not needed from user, will always be localhost:8080
+            write to conf.spoticli in user directory
+
+    
+    '''
     ##load local creds
     ##retrieve token using local creds
     cred = util.config_from_file('conf.spoticli')
